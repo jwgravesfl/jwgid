4130331d99635c5236d85f54adba10d95bc87c5f
J. Graves <jwgravesfl@live.com>
2018-04-13 19:01:49 -0400

    GraphQL connected to front end create-reate-app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+async function loadGreeting() {
+  const response = await fetch('http://localhost:9000/graphql', {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({query: '{ greeting }'})
+  })
+  const responseBody = await response.json()
+  return responseBody.data.greeting
+}
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {greeting: ''}
+    loadGreeting().then((greeting) => this.setState({greeting}))
+  }
+
+
   render() {
+    const {greeting} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="">
+        <header className="">
+          <h1 className="">{greeting}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
